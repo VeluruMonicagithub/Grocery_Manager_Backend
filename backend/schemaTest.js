085f56be89b53c93a56cf43b5ca19c6ca4e7207f
veluru Monica <monicaveluru1@gmail.com>
2026-03-01 14:03:20 +0530

    Updated

--- backend/schemaTest.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("https://lnmfsqxfakuuwpvixqnf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxubWZzcXhmYWt1dXdwdml4cW5mIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MTY1NjYzNywiZXhwIjoyMDg3MjMyNjM3fQ.-m_pwUUBBOiPdXjhbLF-O1TIW_u6mhwWKzpJ4FGTxqs");
-async function run() {
-    let res = await supabase.from("shopping_history").select("*").limit(1);
-    console.log("HISTORY_DATA", res.data);
-}
-run();
